35b84eb1e2b2cad997109c7f71babdda3f145e6e
Anaghasatish2010 <anaghasatish2010@gmail.com>
2025-05-20 17:59:02 +0530

    sixth commit

---
+++ list.py
@@ -0,0 +1,84 @@
+# list_of_names = ["Vladimiras", "Bob","Michael", "Jack","Maria"]
+
+# is_running = True
+# counter = 0
+# while is_running:
+#     print(list_of_names[counter])
+
+#     counter = counter + 1
+#     if counter >= 4:
+#         is_running = False
+
+# list_of_names = ["Vladimiras", "Bob","Michael", "Jack","Maria"]
+
+# is_running = True
+# counter = 0
+# while is_running:
+#     print(list_of_names[counter])
+
+#     counter = counter + 1
+#     if counter >= 5:
+#         is_running = False
+
+# list_of_names = ["Vladimiras", "Bob","Michael", "Jack","Maria"]
+
+# is_running = True
+# counter = 0
+# while is_running:
+#     print(list_of_names[counter])
+
+#     counter = counter + 1
+#     if counter >= len(list_of_names):
+#         is_running = False
+
+# list_of_names = ["Vladimiras", "Bob","Michael", "Jack","Maria"]
+# print(len(list_of_names))
+
+# is_running = True
+# counter = 0
+# while is_running:
+#     print(list_of_names[counter])
+
+#     counter = counter + 1
+#     if counter >= len(list_of_names):
+#         is_running = False
+
+# import turtle
+
+# list_of_colors = ["cyan", "lime", "pink", "yellow", "red"]
+
+# bob = turtle.Turtle()
+# bob.shape("turtle")
+
+# color = 0
+# while True:
+#     bob.color(list_of_colors[color])
+#     bob.left(180)
+
+#     color = color + 1
+
+
+import turtle
+
+list_of_colors = ["cyan", "lime", "pink", "yellow", "red"]
+
+bob = turtle.Turtle()
+bob.shape("turtle")
+
+color = 0
+while True:
+    bob.color(list_of_colors[color])
+    bob.left(180)
+
+    color = color + 1
+    if color >= len(list_of_colors):
+       color = 0
+
+
+
+
+
+
+
+
+
